5804c30eefc772fe6bd06660b117aa5bc98593c8
Kartik <nzokdat@gmail.com>
2018-07-03 22:44:10 +0800

    feature(ui-improvement) : Updated search page and api page

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,18 +1,48 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
-  MatListModule, MatButtonModule, MatCheckboxModule,
-  MatToolbarModule, MatIconModule, MatMenuModule,
-  MatCardModule, MatChipsModule,
-  MatSidenavModule } from '@angular/material';
-
+  MatListModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatMenuModule,
+  MatCardModule,
+  MatChipsModule,
+  MatSidenavModule,
+  MatTabsModule,
+  MatSelectModule,
+  MatGridListModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MatCardModule, MatButtonModule, MatCheckboxModule,
-    MatToolbarModule, MatIconModule, MatMenuModule, MatChipsModule,
-    MatSidenavModule, MatListModule],
-  exports: [MatCardModule, MatButtonModule,
-    MatCheckboxModule, MatToolbarModule, MatIconModule, MatChipsModule,
-    MatMenuModule, MatSidenavModule, MatListModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatChipsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatGridListModule
+  ],
+  exports: [
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatChipsModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatGridListModule
+  ]
 })
-export class MaterialModule { }
+export class MaterialModule {}
