ae2fcec75e6d192e83e8a98f022b8af8a0579cde
Kartik <nzokdat@gmail.com>
2018-07-03 22:50:14 +0800

    fix(search) : Search now uses the new apis/search?query endpoint

--- src/app/core/services/api-info/api-info.service.ts
+++ src/app/core/services/api-info/api-info.service.ts
@@ -12,49 +12,43 @@ import { environment } from '../../../../environments/environment';
 const apiInfoUrl = `${environment.API_ROOT}/apis`;
 @Injectable()
 export class ApiInfoService {
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    getAll(): Observable<ApiInfoList> {
-        return this.http.get<ApiInfoList>(apiInfoUrl)
-        .pipe(
-            tap(heroes => console.log(`fetched apis`)),
-            catchError(this.handleError('getAll', new ApiInfoList()))
-        );
+  getAll(): Observable<ApiInfoList> {
+    return this.http.get<ApiInfoList>(apiInfoUrl).pipe(
+      tap(heroes => console.log(`fetched apis`)),
+      catchError(this.handleError('getAll', new ApiInfoList()))
+    );
+  }
+
+  search(term: string): Observable<ApiInfoList> {
+    if (!term.trim()) {
+      // if not search term, return all
+      return this.http.get<ApiInfoList>(apiInfoUrl);
     }
+    const url = `${apiInfoUrl}/search?query=${term}`;
+    return this.http.get<ApiInfoList>(url).pipe(
+      tap(heroes => console.log(`fetched apis`)),
+      catchError(this.handleError('getAll', new ApiInfoList()))
+    );
+  }
 
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
 
-    search(term: string): Observable<ApiInfoList> {
-        if (!term.trim()) {
-            // if not search term, return all
-            return this.http.get<ApiInfoList>(apiInfoUrl);
-        }
-        const url = `${apiInfoUrl}?query=${term}`;
-        return this.http.get<ApiInfoList>(url)
-            .pipe(
-                tap(heroes => console.log(`fetched apis`)),
-                catchError(this.handleError('getAll', new ApiInfoList))
-            );
-    }
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
 
-    /**
-* Handle Http operation that failed.
-* Let the app continue.
-* @param operation - name of the operation that failed
-* @param result - optional value to return as the observable result
-*/
-    private handleError<T>(operation = 'operation', result?: T) {
-        return (error: any): Observable<T> => {
-
-            // TODO: send the error to remote logging infrastructure
-            console.error(error); // log to console instead
-
-            // TODO: better job of transforming error for user consumption
-            console.log(`${operation} failed: ${error.message}`);
-
-            // Let the app keep running by returning an empty result.
-            return of(result as T);
-
-        };
-    }
-
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
