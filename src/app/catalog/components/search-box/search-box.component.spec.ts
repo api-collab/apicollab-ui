6e67b766f139633fc3082d8d208b1f11db748f0d
Kartik <nzokdat@gmail.com>
2018-07-26 17:31:16 +0800

    style(): Removed unused import

--- src/app/catalog/components/search-box/search-box.component.spec.ts
+++ src/app/catalog/components/search-box/search-box.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed, fakeAsync, inject } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ComponentFixtureAutoDetect } from '@angular/core/testing';
 
