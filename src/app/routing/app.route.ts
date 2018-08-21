424383b6aa5a8d00097bcb0def15e530a10217db
kand617 <kand617@aucklanduni.ac.nz>
2018-08-21 15:15:08 +0800

    refactor(api-page-url) api page url is now /apis/apiid (#25)

--- src/app/routing/app.route.ts
+++ src/app/routing/app.route.ts
@@ -3,18 +3,18 @@ import { ApiPageComponent } from '../api-page/api-page.component';
 import { CatalogComponent } from '../catalog/catalog.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: '/catalog',
-        pathMatch: 'full'
-    },
+  {
+    path: '',
+    redirectTo: '/catalog',
+    pathMatch: 'full'
+  },
 
-    {
-        path: 'catalog',
-        loadChildren: 'app/catalog/catalog.module#CatalogModule',
-    },
-    {
-        path: 'api',
-        loadChildren: 'app/api-page/api-page.module#ApiPageModule',
-    }
+  {
+    path: 'catalog',
+    loadChildren: 'app/catalog/catalog.module#CatalogModule'
+  },
+  {
+    path: 'apis',
+    loadChildren: 'app/api-page/api-page.module#ApiPageModule'
+  }
 ];
