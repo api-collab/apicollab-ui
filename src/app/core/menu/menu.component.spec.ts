7f7e0c8b03505fc9ef36e96e5b943f52f6916170
Kartik <nzokdat@gmail.com>
2018-09-05 17:14:57 +0800

    fix(test) : Fixed failing test case

--- src/app/core/menu/menu.component.spec.ts
+++ src/app/core/menu/menu.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MenuComponent } from './menu.component';
 
@@ -10,7 +11,7 @@ describe('menuComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [SharedModule],
+      imports: [SharedModule, RouterTestingModule],
       declarations: [MenuComponent]
     }).compileComponents();
   }));
