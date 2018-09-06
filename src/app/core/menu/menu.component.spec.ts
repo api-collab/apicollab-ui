2bbd6b18606d9a5ae74a0bb03128591b594a8f8b
Kartik <nzokdat@gmail.com>
2018-09-06 15:30:40 +0800

    ui(purple) :  Minor theme updates

--- src/app/core/menu/menu.component.spec.ts
+++ src/app/core/menu/menu.component.spec.ts
@@ -1,8 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { MenuComponent } from './menu.component';
-
 import { SharedModule } from './../../shared';
 
 describe('menuComponent', () => {
