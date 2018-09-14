c1ebca0ac9cafd52339b1140d4e1107be9db0f92
Kartik <nzokdat@gmail.com>
2018-09-14 23:23:44 +0800

    fix(api-card) : Empty tags are removed from rendering

--- src/app/catalog/components/api-card/api-card.component.spec.ts
+++ src/app/catalog/components/api-card/api-card.component.spec.ts
@@ -4,20 +4,31 @@ import { ApiCardComponent } from './api-card.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SharedModule } from './../../../shared';
 import { CoreModule } from './../../../core';
+import { ApiInfo } from '../../../core/models/api-info';
+import { By } from '@angular/platform-browser';
+import { MatChip } from '@angular/material';
+import { Component } from '@angular/core';
+
+@Component({
+  template: '<app-api-card class="app-api-card" [apiInfo]="apiInfo"></app-api-card>'
+})
+class TestHostComponent {
+  apiInfo: ApiInfo;
+}
 
 describe('ApiCardComponent', () => {
-  let component: ApiCardComponent;
-  let fixture: ComponentFixture<ApiCardComponent>;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let component: TestHostComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, SharedModule, CoreModule],
-      declarations: [ApiCardComponent]
+      declarations: [ApiCardComponent, TestHostComponent]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ApiCardComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -25,4 +36,22 @@ describe('ApiCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should assign the initial value from parent', async(() => {
+    expect(component).toBeTruthy();
+    const dummy = new ApiInfo();
+    dummy.apiId = '1';
+    dummy.applicationId = '2';
+    dummy.desription = 'hello';
+    dummy.name = 'name';
+    dummy.status = 'BETA';
+    dummy.tags = ['hello', '', 'world'];
+    dummy.version = ''; // empty tag
+    component.apiInfo = dummy;
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const elements = fixture.debugElement.queryAll(By.directive(MatChip));
+      expect(elements.length).toBe(2);
+    });
+  }));
 });
