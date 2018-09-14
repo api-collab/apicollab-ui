76289d1e9dbdea7d8eb5ef66794dade69ea1bfba
kand617 <kand617@aucklanduni.ac.nz>
2018-09-14 23:39:36 +0800

    fix(api-card) : Empty tags are removed from rendering (#44)
    * fix(api-card) : Empty tags are removed from rendering
    
    * test(api-card) : Updated test description

--- src/app/catalog/components/api-card/api-card.component.ts
+++ src/app/catalog/components/api-card/api-card.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ApiInfo } from '../../../core/models/api-info';
 @Component({
   selector: 'app-api-card',
   templateUrl: './api-card.component.html',
   styleUrls: ['./api-card.component.scss']
 })
-export class ApiCardComponent implements OnInit {
-  @Input() apiInfo: ApiInfo;
+export class ApiCardComponent implements OnChanges {
+  @Input()
+  apiInfo: ApiInfo;
+  allTags: string[];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges) {
+    const change = changes.apiInfo;
+    if (change && change.currentValue !== change.previousValue) {
+      this.apiInfo = change.currentValue;
+      this.allTags = this.buildTags();
+    }
+  }
+
+  buildTags(): string[] {
+    let tags = [];
+    if (this.apiInfo.tags != null && this.apiInfo.tags.length > 0) {
+      tags = this.apiInfo.tags.filter(
+        value => !(!value || value === undefined || value === '' || value.trim().length === 0)
+      );
+    }
+    let version = this.apiInfo.version;
+    version = version ? version.trim() : '';
+    if (this.apiInfo.version.length > 0) {
+      tags.push(`v${version}`);
+    }
+    return tags;
+  }
 }
